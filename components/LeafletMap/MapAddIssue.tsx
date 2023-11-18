1b3ec97926c6dd00770f171150abefb120bb5e42
RaduTek <radutek13@gmail.com>
2023-11-18 20:45:35 +0200

    Implement account restriction for add issue button

--- components/LeafletMap/MapAddIssue.tsx
+++ components/LeafletMap/MapAddIssue.tsx
@@ -3,9 +3,12 @@ import Control from "react-leaflet-custom-control";
 import { Fab } from "@mui/material";
 import { Add } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { enqueueSnackbar } from "notistack";
 
 export default function MapAddIssue() {
   const router = useRouter();
+  const { status } = useSession();
 
   return (
     <>
@@ -15,7 +18,11 @@ export default function MapAddIssue() {
           size="medium"
           aria-label="locate"
           onClick={() => {
-            router.push("/map/issue/add");
+            if (status === "authenticated") {
+              router.push("/map/issue/add");
+            } else {
+              enqueueSnackbar("You must be logged in to add an issue.");
+            }
           }}
         >
           <Add />

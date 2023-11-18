e749634f5a48e8e97232c6aefbcc0c5bfcb0ba75
RaduTek <radutek13@gmail.com>
2023-11-19 00:56:28 +0200

    Create new index layout

---
+++ app/indexLayout.tsx
@@ -0,0 +1,48 @@
+"use client";
+import {
+  AppBar,
+  Box,
+  Button,
+  CssBaseline,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { SnackbarProvider } from "notistack";
+import { AccountCircle, Map } from "@mui/icons-material";
+import Link from "next/link";
+
+export default function IndexLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <SnackbarProvider maxSnack={2} preventDuplicate />
+        <CssBaseline />
+        <AppBar position="fixed" component="nav" sx={{ zIndex: 1300 }}>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              CitySavers
+            </Typography>
+            <Link href="/map">
+              <Button endIcon={<Map />} sx={{ color: "#fff" }}>
+                Map
+              </Button>
+            </Link>
+            <Link href="/login">
+              <Button endIcon={<AccountCircle />} sx={{ color: "#fff" }}>
+                Login
+              </Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+        <Box component="main" sx={{ p: { xs: 2, sm: 3 }, width: "100dvw" }}>
+          <Toolbar />
+          {children}
+        </Box>
+      </Box>
+    </>
+  );
+}

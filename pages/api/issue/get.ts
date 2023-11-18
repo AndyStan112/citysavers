456670b3fbdb411d672a4156ea40db25739a9ce8
Dragan Iulian <iulianalexandrudragan@gmail.com>
2023-11-18 18:50:28 +0200

    add /api/issue/id/[id] route

--- pages/api/issue/get.ts
+++ pages/api/issue/get.ts
@@ -10,7 +10,7 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   try{
-    const issues = await prisma.issue.findMany();
+    const issues = await prisma.issue.findMany({where:{status:'approved'}});
     res.status(200).json(issues);
   }catch(e){
     res.status(400).json({error: e})

cbe4ffe222a75bdb9e39a66197dd4db418d7ab01
RaduTek <radutek13@gmail.com>
2023-11-19 00:56:22 +0200

    Remove old index route group

--- app/(index)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Card, Typography } from "@mui/material";
-
-export default function LoginPage() {
-  return (
-    <Card sx={{ marginX: "auto", maxWidth: "400px" }}>
-      <Typography variant="h6">Login</Typography>
-    </Card>
-  );
-}
--- app/(index)/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function IndexPage() {
-  return <Typography variant="h4">Welcome to CitySavers!</Typography>;
-}

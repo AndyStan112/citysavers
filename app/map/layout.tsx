4c96461deca8594a14bcb3f6b8149fd3db64c1c1
RaduTek <radutek13@gmail.com>
2023-11-18 09:49:46 +0200

    Move children outlet position

--- app/map/layout.tsx
+++ app/map/layout.tsx
@@ -13,10 +13,10 @@ export default function UserMapLayout({
     <>
       <CssBaseline />
       <Navbar />
+      {children}
       <LeafletMap>
         <SearchBox />
       </LeafletMap>
-      {children}
     </>
   );
 }

31d39b4303d9cd466dea6e19b4b2853f62dd7d08
AndyStan112 <stanica.andrei28@gmail.com>
2023-11-18 17:04:31 +0200

    add skeleton for issue api routes

---
+++ pages/api/issue/id/[id].ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from "../../../../lib/prismadb"
+type Data = {
+  name: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const id =  req.query.id;
+  res.status(200).json({ name: 'John Doe' })
+}

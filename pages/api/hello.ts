c597c17b5fc320637c14d1702b28824fc88f4c8f
AndyStan112 <stanica.andrei28@gmail.com>
2023-11-19 00:52:21 +0200

    hello

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -13,9 +13,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  //const { image } = req.body;
-  const image =
-    "https://lh3.googleusercontent.com/a/ACg8ocKs_gmz-cHo90Q18-J1iSjm58dJoEiCh2kRBoi3YRD7sPGy=s96-c";
+  const { images } = req.body;
+  const image = images[0];
+  // const image =
+  //   "https://lh3.googleusercontent.com/a/ACg8ocKs_gmz-cHo90Q18-J1iSjm58dJoEiCh2kRBoi3YRD7sPGy=s96-c";
   const fileName = uuidv4() + ".png";
   const file = bucket.file(fileName);
   const writeStream = file.createWriteStream();

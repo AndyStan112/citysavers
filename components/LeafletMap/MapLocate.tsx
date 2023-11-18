bf10a825e79be1133d2d41e450efeee6dab91354
RaduTek <radutek13@gmail.com>
2023-11-18 09:49:10 +0200

    Remove add issue button from locate component

--- components/LeafletMap/MapLocate.tsx
+++ components/LeafletMap/MapLocate.tsx
@@ -1,20 +1,15 @@
 "use client";
 import Control from "react-leaflet-custom-control";
-import { Fab, Stack } from "@mui/material";
-import { LocationSearching, Add } from "@mui/icons-material";
+import { Fab } from "@mui/material";
+import { LocationSearching } from "@mui/icons-material";
 
 export default function MapLocate() {
   return (
     <>
       <Control prepend position="bottomright">
-        <Stack gap={1.5}>
-          <Fab color="info" size="medium" aria-label="locate">
-            <LocationSearching />
-          </Fab>
-          <Fab color="secondary" size="medium" aria-label="locate">
-            <Add />
-          </Fab>
-        </Stack>
+        <Fab color="info" size="medium" aria-label="locate">
+          <LocationSearching />
+        </Fab>
       </Control>
     </>
   );

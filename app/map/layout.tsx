bf7b0ba5230a84505726693e94a0bbbfa251e60c
RaduTek <radutek13@gmail.com>
2023-11-18 20:44:54 +0200

    Change snackbar config

--- app/map/layout.tsx
+++ app/map/layout.tsx
@@ -11,7 +11,7 @@ export default function UserMapLayout({
 }) {
   return (
     <>
-      <SnackbarProvider />
+      <SnackbarProvider maxSnack={2} preventDuplicate />
       <CssBaseline />
       <Navbar />
       {children}

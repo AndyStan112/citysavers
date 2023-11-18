d8f34248ad4985042ee4932197ffeb4725c05e18
AndyStan112 <stanica.andrei28@gmail.com>
2023-11-18 22:40:23 +0200

    add backend routes for admin board

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,19 +1,44 @@
-'use client'
+"use client";
 import { Button } from "@mui/material";
-import { useSession } from "next-auth/react"
-import { signIn } from 'next-auth/react';
+import { useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 export default function UserMap() {
   const redirectUrl = process.env.NEXTAUTH_URL;
-const logIn = (provider:string, email =undefined) => {
-  email
-    ? signIn(provider, {}, email)
-    : signIn(provider, {});
-};
-    const {data,status} = useSession()
-   
-    return <div>
-    {JSON.stringify(data)}
-     <Button onClick={()=>{logIn("google")}}>test</Button>
-    </div>
+  const { data, status } = useSession();
+  const logIn = (provider: string, email = undefined) => {
+    email ? signIn(provider, {}, email) : signIn(provider, {});
+  };
 
+  return (
+    <div>
+      {JSON.stringify(data)}
+      <Button
+        onClick={() => {
+          logIn("google");
+        }}
+      >
+        test
+      </Button>
+      <Button
+        onClick={async () => {
+          await fetch("/api/issue/admin/get?type=all", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ id: data?.user.id }),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }}
+      >
+        TestAdmin
+      </Button>
+    </div>
+  );
 }
---
+++ constants/DbConstants.ts
@@ -0,0 +1,1 @@
+export const STATUSES = ["rejected", "pending", "approved", "solved"];

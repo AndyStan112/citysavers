652ce1d10392bc3920ffe9634c43abd7b8265aa1
AndyStan112 <stanica.andrei28@gmail.com>
2023-11-18 17:30:33 +0200

    hardcoded model for issue/add

--- pages/api/issue/add.ts
+++ pages/api/issue/add.ts
@@ -9,5 +9,20 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const {userId,category,location,moreDetails,shortDescription,photos,locationType,priority }= req.body;
+  const [latitude,longitude]= location;
+  await prisma.issue.create({
+    data:{
+      reportedBy:userId,
+      latitude,
+      longitude,
+      locationType,
+      category,
+      priority,
+      shortDescription,
+      moreDetails,
+      photosUrl:["https://lh3.googleusercontent.com/a/ACg8ocKs_gmz-cHo90Q18-J1iSjm58dJoEiCh2kRBoi3YRD7sPGy=s96-c","https://lh3.googleusercontent.com/a/ACg8ocKs_gmz-cHo90Q18-J1iSjm58dJoEiCh2kRBoi3YRD7sPGy=s96-c"]
+
+  }})
   res.status(200).json({ name: 'John Doe' })
 }

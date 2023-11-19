4cac3021d8ad1671b7e6a7d415b914a358fe7197
Dragan Iulian <iulianalexandrudragan@gmail.com>
2023-11-19 10:54:05 +0200

    update prisma schema

--- prisma/migrations/20231119085338_/migration.sql
+++ prisma/migrations/20231119085338_/migration.sql
@@ -22,7 +22,7 @@ CREATE TABLE "Session" (
     "id" TEXT NOT NULL,
     "sessionToken" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
-    "expires" TIMESTAMP(3) NOT NULL,
+    "expires" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
 );
@@ -48,14 +48,14 @@ CREATE TABLE "Issue" (
     "longitude" DOUBLE PRECISION NOT NULL,
     "latitude" DOUBLE PRECISION NOT NULL,
     "status" TEXT NOT NULL DEFAULT 'pending',
-    "statusMessage" TEXT NOT NULL,
+    "statusMessage" TEXT,
+    "category" TEXT NOT NULL,
     "photosUrl" TEXT[],
-    "dateAdded" TIMESTAMP(3) NOT NULL,
+    "dateAdded" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "locationType" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
+    "moreDetails" TEXT,
     "shortDescription" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
-    "issueCategoryId" TEXT NOT NULL,
     "priority" TEXT NOT NULL,
 
     CONSTRAINT "Issue_pkey" PRIMARY KEY ("id")
@@ -78,7 +78,7 @@ CREATE TABLE "Solution" (
 CREATE TABLE "Transaction" (
     "id" TEXT NOT NULL,
     "coins" INTEGER NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
+    "date" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "issueId" TEXT,
     "solutionId" TEXT,
 
@@ -86,21 +86,10 @@ CREATE TABLE "Transaction" (
 );
 
 -- CreateTable
-CREATE TABLE "IssueCategory" (
-    "id" TEXT NOT NULL,
-    "shortName" TEXT NOT NULL,
-    "longName" TEXT NOT NULL,
-    "icon" TEXT NOT NULL,
-    "isCommunitySolvable" BOOLEAN NOT NULL,
-
-    CONSTRAINT "IssueCategory_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "VerificationToken" (
     "identifier" TEXT NOT NULL,
     "token" TEXT NOT NULL,
-    "expires" TIMESTAMP(3) NOT NULL
+    "expires" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateIndex
@@ -132,9 +121,6 @@ ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId"
 
 -- AddForeignKey
 ALTER TABLE "Issue" ADD CONSTRAINT "Issue_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Issue" ADD CONSTRAINT "Issue_issueCategoryId_fkey" FOREIGN KEY ("issueCategoryId") REFERENCES "IssueCategory"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Solution" ADD CONSTRAINT "Solution_issueId_fkey" FOREIGN KEY ("issueId") REFERENCES "Issue"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

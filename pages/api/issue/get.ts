3dba117419f97bdee8950e877f160600235703d8
AndyStan112 <stanica.andrei28@gmail.com>
2023-11-19 13:32:00 +0200

    fixed some type errors

--- pages/api/issue/get.ts
+++ pages/api/issue/get.ts
@@ -1,18 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import prisma from "../../../lib/prismadb"
-type Data = {
-  name: string
-}
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prismadb";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  try{
-    const issues = await prisma.issue.findMany({where:{status:'approved'}});
+  try {
+    const issues = await prisma.issue.findMany({
+      where: { status: "approved" },
+    });
     res.status(200).json(issues);
-  }catch(e){
-    res.status(400).json({error: e})
+  } catch (e) {
+    res.status(400).json({ error: e });
   }
 }

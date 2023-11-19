a08a600c36216a124ccccc69f034b2ba1e20860e
RaduTek <radutek13@gmail.com>
2023-11-19 10:05:36 +0200

    Implement data fetching

--- app/map/issue/[id]/page.tsx
+++ app/map/issue/[id]/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import ChipsList from "@/components/ChipsList/ChipsList";
 import Gallery from "@/components/Gallery/Gallery";
 import OverlayPage from "@/components/OverlayPage/OverlayPage";
@@ -13,77 +14,113 @@ import {
   Share,
 } from "@mui/icons-material";
 import { Avatar, Button, Chip, Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { enqueueSnackbar } from "notistack";
 
 export default function ViewIssuePage({ params }: { params: { id: string } }) {
+  const [issueData, setIssueData] = useState<any>(null);
+
+  useEffect(() => {
+    if (!issueData) {
+      fetch("/api/issue/id/" + params.id)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setIssueData(data);
+        })
+        .catch((error) => {
+          console.log(error);
+          enqueueSnackbar(`Issue with id: ${params.id} not found!`);
+        });
+    }
+  }, [issueData, params.id]);
+
   return (
     <OverlayPage>
-      <Typography variant="h6">Short description</Typography>
-      <ChipsList>
-        <Chip
-          icon={<AccessAlarm />}
-          label="Pending approval"
-          size="small"
-          color="warning"
-        />
-        <Chip
-          icon={<DoNotDisturbOn />}
-          label="Rejected"
-          size="small"
-          color="error"
-        />
-        <Chip
-          icon={<AccessAlarm />}
-          label="Pending solution"
-          size="small"
-          color="info"
-        />
-        <Chip icon={<Done />} label="Solved" size="small" color="success" />
-        <Chip icon={<DirectionsBus />} label="Bus Station" size="small" />
-        <Chip icon={<DoNotStep />} label="Vandalism" size="small" />
-        <Chip avatar={<Avatar>AU</Avatar>} label="App User" size="small" />
-      </ChipsList>
-      <Typography>Images:</Typography>
-      <Gallery
-        imageList={[
-          "/images/placeholder.webp",
-          "/images/placeholder.webp",
-          "/images/placeholder.webp",
-        ]}
-      />
-      <Typography>
-        <strong>Description:</strong>
-        <br />
-        This is the description of this issue.
-      </Typography>
-      <Button color="primary" variant="contained" fullWidth startIcon={<Add />}>
-        Add solution
-      </Button>
-      <Stack direction="row" gap={1}>
-        <Button
-          color="success"
-          variant="outlined"
-          fullWidth
-          startIcon={<Launch />}
-        >
-          GMaps
-        </Button>
-        <Button
-          color="primary"
-          variant="outlined"
-          fullWidth
-          startIcon={<BookmarkBorder />}
-        >
-          Save
-        </Button>
-        <Button
-          color="secondary"
-          variant="outlined"
-          fullWidth
-          startIcon={<Share />}
-        >
-          Share
-        </Button>
-      </Stack>
+      {issueData ? (
+        <>
+          <Typography variant="h6">{issueData.shortDescription}</Typography>
+          <ChipsList>
+            <Chip
+              icon={<AccessAlarm />}
+              label="Pending approval"
+              size="small"
+              color="warning"
+            />
+            <Chip
+              icon={<DoNotDisturbOn />}
+              label="Rejected"
+              size="small"
+              color="error"
+            />
+            <Chip
+              icon={<AccessAlarm />}
+              label="Pending solution"
+              size="small"
+              color="info"
+            />
+            <Chip icon={<Done />} label="Solved" size="small" color="success" />
+            <Chip icon={<DirectionsBus />} label="Bus Station" size="small" />
+            <Chip icon={<DoNotStep />} label="Vandalism" size="small" />
+            <Chip avatar={<Avatar>AU</Avatar>} label="App User" size="small" />
+          </ChipsList>
+          <Typography>Images:</Typography>
+          <Gallery imageList={issueData.photosUrl} />
+          <Typography>
+            <strong>More details:</strong>
+            <br />
+            {issueData.moreDetails}
+          </Typography>
+          <Button
+            color="primary"
+            variant="contained"
+            fullWidth
+            startIcon={<Add />}
+          >
+            Add solution
+          </Button>
+          <Stack direction="row" gap={1}>
+            <a
+              href={
+                "https://maps.google.com/maps?z=12&t=m&q=loc:" +
+                issueData.latitude +
+                "+" +
+                issueData.longitude
+              }
+              target="_blank"
+            >
+              <Button
+                color="success"
+                variant="outlined"
+                fullWidth
+                startIcon={<Launch />}
+              >
+                GMaps
+              </Button>
+            </a>
+            <Button
+              color="primary"
+              variant="outlined"
+              fullWidth
+              startIcon={<BookmarkBorder />}
+              disabled
+            >
+              Save
+            </Button>
+            <Button
+              color="secondary"
+              variant="outlined"
+              fullWidth
+              startIcon={<Share />}
+              disabled
+            >
+              Share
+            </Button>
+          </Stack>
+        </>
+      ) : (
+        <Typography>Loading</Typography>
+      )}
     </OverlayPage>
   );
 }

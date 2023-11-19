e4a79314537e799d7b92a8885b45ed31c33879a1
AndyStan112 <stanica.andrei28@gmail.com>
2023-11-19 12:03:18 +0200

    add solution modify api route

--- pages/api/admin/solution/modify/[status].ts
+++ pages/api/admin/solution/modify/[status].ts
@@ -1,15 +1,16 @@
+//@ts-nocheck
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/lib/prismadb";
 import { isAdmin } from "@/lib/util";
-import { P_TO_ISSUE } from "@/constants/DbConstants";
+import { P_TO_ISSUE, P_TO_SOLUTION } from "@/constants/DbConstants";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const { userId, issueId, priority } = req.body;
+    const { userId, solutionId, issueId, priority } = req.body;
     const status = req.query.status as string;
     if (!["rejected", "approved"].includes(status))
       throw new Error("Status not allowed");
@@ -18,41 +19,48 @@ export default async function handler(
     // const userId = "clp59dk0d000ahs6b0hgvaoxg";`
     //const status== approved
     if (!isAdmin(userId)) throw new Error("User not admin");
-    // @ts-ignore
-    const coins = P_TO_ISSUE[priority];
+    const coins = P_TO_SOLUTION[priority];
     console.log(coins);
     const STATUS_TO_COIN_CHANGE = {
       rejected: { decrement: coins },
       approved: { increment: coins },
     };
     const sign = status === "approved" ? 1 : -1;
-    const issue = await prisma.issue.findUnique({
+    const solution = await prisma.issue.findUnique({
       where: { id: issueId },
-      include: { reportedBy: true },
+      select: { userId: true },
     });
 
-    if (!issue) {
-      throw new Error(`Issue with ID ${issueId} not found.`);
+    if (!solution) {
+      throw new Error(`Issue with ID ${solutionId} not found.`);
     }
 
-    const reportedById = issue.reportedBy.id;
-    await prisma.$transaction([
-      prisma.issue.update({
-        where: { id: issueId },
+    const solverId = solution.userId;
+    let transation = [
+      prisma.solution.update({
+        where: { id: solutionId },
         data: { status },
       }),
       prisma.user.update({
-        where: { id: reportedById },
-        //@ts-ignore
+        where: { id: solverId },
         data: { coins: STATUS_TO_COIN_CHANGE[status] },
       }),
       prisma.transaction.create({
         data: {
           coins: coins * sign,
-          issue: { connect: { id: issueId } },
+          solution: { connect: { id: solutionId } },
         },
       }),
-    ]);
+    ];
+    if (status === "approved")
+      transation = [
+        ...transation,
+        prisma.issue.update({
+          where: { id: issueId },
+          data: { status },
+        }),
+      ];
+    await prisma.$transaction(transation);
     res.status(200).json({ name: "John Doe" });
   } catch (e: any) {
     res.status(400).json({ error: e.message });

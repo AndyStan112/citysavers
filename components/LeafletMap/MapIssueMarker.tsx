e6daadb873066536d0f2bfb1c73d132cca1c92dd
RaduTek <radutek13@gmail.com>
2023-11-19 12:07:44 +0200

    Add Issue Marker component

---
+++ components/LeafletMap/MapIssueMarker.tsx
@@ -0,0 +1,45 @@
+import { Marker } from "react-leaflet";
+import {
+  AbandonedVehicleIcon,
+  BlockedRoadIcon,
+  LitterIcon,
+  VandalismIcon,
+} from "@/constants/MapIcons";
+import { useRouter } from "next/navigation";
+
+export default function IssueMarker({
+  key,
+  latitude,
+  longitude,
+  issueId,
+  category,
+}: {
+  key: number;
+  latitude: number;
+  longitude: number;
+  issueId: string;
+  category: string;
+}) {
+  const router = useRouter();
+
+  const markerClickHandler = () => {
+    router.push("/map/issue/" + issueId);
+  };
+
+  return (
+    <Marker
+      key={key}
+      position={[latitude, longitude]}
+      icon={
+        category == "abandoned-vehicle"
+          ? AbandonedVehicleIcon
+          : category == "blocked-path"
+          ? BlockedRoadIcon
+          : category == "litter"
+          ? LitterIcon
+          : VandalismIcon
+      }
+      eventHandlers={{ click: markerClickHandler }}
+    />
+  );
+}

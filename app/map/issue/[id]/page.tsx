68a035be9a6aeb2148b668fb7013b2f04a470df2
Dragan Iulian <iulianalexandrudragan@gmail.com>
2023-11-19 13:13:36 +0200

    add link from issue to add solution

--- app/map/issue/[id]/page.tsx
+++ app/map/issue/[id]/page.tsx
@@ -16,10 +16,10 @@ import { useEffect, useState } from "react";
 import { enqueueSnackbar } from "notistack";
 import { LocationTypesData } from "@/constants/LocationTypes";
 import { IssueTypesData } from "@/constants/IssueTypes";
+import Link from "next/link";
 
 export default function ViewIssuePage({ params }: { params: { id: string } }) {
   const [issueData, setIssueData] = useState<any>(null);
-
   useEffect(() => {
     if (!issueData) {
       fetch("/api/issue/id/" + params.id)
@@ -116,14 +116,16 @@ export default function ViewIssuePage({ params }: { params: { id: string } }) {
             {issueData.moreDetails}
           </Typography>
           {issueData.status == "approved" ? (
-            <Button
-              color="primary"
-              variant="contained"
-              fullWidth
-              startIcon={<Add />}
-            >
-              Add solution
-            </Button>
+            <Link href={`/map/solution/add/${params.id}`}>
+              <Button
+                color="primary"
+                variant="contained"
+                fullWidth
+                startIcon={<Add />}
+              >
+                Add solution
+              </Button>
+            </Link>
           ) : (
             <></>
           )}

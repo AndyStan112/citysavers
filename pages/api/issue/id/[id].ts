042ee8fd0f1ec77e07bd3c6aba70ea56e18baba6
RaduTek <radutek13@gmail.com>
2023-11-19 10:03:22 +0200

    Fix types

--- pages/api/issue/id/[id].ts
+++ pages/api/issue/id/[id].ts
@@ -1,15 +1,30 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import prisma from "../../../../lib/prismadb";
+import prisma from "@/lib/prismadb";
+type Data = {
+  id: string;
+  longitude: number;
+  latitude: number;
+  status: string;
+  statusMessage: string;
+  category: string;
+  photosUrl: string[];
+  dateAdded: Date | null;
+  locationType: string;
+  moreDetails: string | null;
+  shortDescription: string;
+  userId: string;
+  priority: string;
+  issueCategoryId: string | null;
+};
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<Data | { error: string }>
 ) {
   const id = req.query.id as string;
   try {
-    const issue = await prisma.issue.findUnique({ where: { id: id } });
-    if (!issue) throw new Error("No issues with id found");
+    const issue = await prisma.issue.findUnique({ where: { id } });
+    if (!issue) throw new Error("Not found");
     res.status(200).json(issue);
   } catch (e: any) {
     res.status(400).json({ error: e.message });

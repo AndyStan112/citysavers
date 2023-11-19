a4838bc41ab97ae12dcbec9082a814ca2d379b35
RaduTek <radutek13@gmail.com>
2023-11-19 03:14:21 +0200

    Create sample near me page

---
+++ app/map/nearme/page.tsx
@@ -0,0 +1,10 @@
+import OverlayPage from "@/components/OverlayPage/OverlayPage";
+import { Typography } from "@mui/material";
+
+export default function NearMePage() {
+  return (
+    <OverlayPage>
+      <Typography variant="h6">Issues open near me</Typography>
+    </OverlayPage>
+  );
+}

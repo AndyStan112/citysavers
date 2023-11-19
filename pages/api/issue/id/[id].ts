336e15ea0b9d22b078f2ddec96a3323671d91069
Dragan Iulian <iulianalexandrudragan@gmail.com>
2023-11-19 09:44:14 +0200

    fix /api/issue/id/[id] route

--- pages/api/issue/id/[id].ts
+++ pages/api/issue/id/[id].ts
@@ -1,20 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import prisma from "../../../../lib/prismadb"
-type Data = {
-  name: string
-}
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../../lib/prismadb";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  const id =  req.query.id;
-  try{
-    const issue = await prisma.issue.findUnique({where: {id}});
-    if(!issue)throw new Error("No issues with id found")
+  const id = req.query.id as string;
+  try {
+    const issue = await prisma.issue.findUnique({ where: { id: id } });
+    if (!issue) throw new Error("No issues with id found");
     res.status(200).json(issue);
-  }catch(e:any){
-    res.status(400).json({error: e.message})
+  } catch (e: any) {
+    res.status(400).json({ error: e.message });
   }
 }

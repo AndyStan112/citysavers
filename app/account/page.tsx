19f387944811ea7bdbee7c0157217d4c5d993438
RaduTek <radutek13@gmail.com>
2023-11-19 02:15:39 +0200

    Add account page with sign out button.

---
+++ app/account/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Logout } from "@mui/icons-material";
+import {
+  Box,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  MenuList,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { signOut } from "next-auth/react";
+import { enqueueSnackbar } from "notistack";
+
+export default function AccountPage() {
+  const logoutHandler = async () => {
+    try {
+      await signOut({ callbackUrl: "/" });
+    } catch (error) {
+      console.log(error);
+      enqueueSnackbar("Failed to log you out.");
+    }
+  };
+
+  return (
+    <Box sx={{ marginX: "auto", maxWidth: "900px" }}>
+      <Paper>
+        <MenuList>
+          <MenuItem onClick={logoutHandler}>
+            <ListItemIcon>
+              <Logout />
+            </ListItemIcon>
+            <ListItemText>Log out</ListItemText>
+          </MenuItem>
+        </MenuList>
+      </Paper>
+    </Box>
+  );
+}

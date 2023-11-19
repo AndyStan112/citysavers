a0b9dec73be8050e40902544e08edd17b8d5be93
AndyStan112 <stanica.andrei28@gmail.com>
2023-11-19 10:47:05 +0200

    add token  and severity mappings

--- constants/DbConstants.ts
+++ constants/DbConstants.ts
@@ -1,1 +1,11 @@
 export const STATUSES = ["rejected", "pending", "approved", "solved"];
+export const P_TO_ISSUE = {
+  low: 5,
+  medium: 10,
+  high: 20,
+};
+export const P_TO_SOLUTION = {
+  low: 30,
+  medium: 60,
+  high: 120,
+};

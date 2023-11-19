1382119b147b7c24f13c5e033e7c1a9d082e645a
AndyStan112 <stanica.andrei28@gmail.com>
2023-11-19 12:55:20 +0200

    copy add

---
+++ pages/api/solution/add.ts
@@ -0,0 +1,41 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/lib/prismadb";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const {
+      userId,
+      category,
+      latitude,
+      longitude,
+      moreDetails,
+      shortDescription,
+      photosUrl,
+      locationType,
+      priority,
+    } = req.body;
+    const newIssue = await prisma.issue.create({
+      data: {
+        userId,
+        latitude,
+        longitude,
+        locationType,
+        category,
+        priority,
+        shortDescription,
+        moreDetails,
+        statusMessage: "",
+        photosUrl: photosUrl,
+      },
+    });
+    res
+      .status(200)
+      .json({ id: newIssue.id, message: "issue successfully created" });
+  } catch (e: any) {
+    res.status(400).json({ error: e.message });
+  }
+}

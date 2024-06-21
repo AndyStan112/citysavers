1d5d49e101bf29c1cdb779985932885913526c59
RaduTek <radutek13@gmail.com>
2024-06-21 21:33:40 +0300

    User profile and marketplace

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,12 +1,15 @@
 "use client";
-import { Logout } from "@mui/icons-material";
+import { Logout, Store } from "@mui/icons-material";
 import {
+  Avatar,
   Box,
+  Divider,
   ListItemIcon,
   ListItemText,
   MenuItem,
   MenuList,
   Paper,
+  Stack,
   Typography,
 } from "@mui/material";
 import { signOut, useSession } from "next-auth/react";
@@ -15,7 +18,7 @@ import { enqueueSnackbar } from "notistack";
 import { useEffect } from "react";
 
 export default function AccountPage() {
-  const { status } = useSession();
+  const { update, data, status } = useSession();
   const router = useRouter();
 
   useEffect(() => {
@@ -33,18 +36,41 @@ export default function AccountPage() {
     }
   };
 
-  return (
-    <Box sx={{ marginX: "auto", maxWidth: "900px" }}>
-      <Paper>
-        <MenuList>
-          <MenuItem onClick={logoutHandler}>
-            <ListItemIcon>
-              <Logout />
-            </ListItemIcon>
-            <ListItemText>Log out</ListItemText>
-          </MenuItem>
-        </MenuList>
+  return data ? (
+    <Box sx={{ marginX: "auto", maxWidth: "500px" }}>
+      <Paper sx={{ marginTop: "75px" }}>
+        <Stack gap={0.5}>
+          <Stack alignItems="center" gap={1.5}>
+            <Avatar
+              sx={{ width: "150px", height: "150px", marginTop: "-75px" }}
+              alt={data.user.name ? data.user.name : "Unknown"}
+              src={data.user.image ? data.user.image : ""}
+            />
+            <Typography variant="h5">
+              {data.user.name ? data.user.name : "Unknown"}
+            </Typography>
+            <Typography>0 coins</Typography>
+          </Stack>
+          <Divider component="hr" />
+          <MenuList>
+            <MenuItem onClick={() => router.push("/account/marketplace")}>
+              <ListItemIcon>
+                <Store />
+              </ListItemIcon>
+              <ListItemText>Marketplace</ListItemText>
+            </MenuItem>
+
+            <MenuItem onClick={logoutHandler}>
+              <ListItemIcon>
+                <Logout />
+              </ListItemIcon>
+              <ListItemText>Log out</ListItemText>
+            </MenuItem>
+          </MenuList>
+        </Stack>
       </Paper>
     </Box>
+  ) : (
+    <></>
   );
 }

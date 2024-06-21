96898fbffe8eb411f8bc31a84d4c7f159015ecef
RaduTek <radutek13@gmail.com>
2024-06-22 00:44:50 +0300

    Saved list

--- app/map/saved/page.tsx
+++ app/map/saved/page.tsx
@@ -1,61 +1,70 @@
+"use client";
+import { IssueData } from "@/components/IssueListItem/IssueData";
+import IssueListItem from "@/components/IssueListItem/IssueListItem";
 import OverlayPage from "@/components/OverlayPage/OverlayPage";
-import { AccessAlarm, DeleteOutline } from "@mui/icons-material";
 import {
-  Chip,
+  Box,
   Divider,
   List,
-  ListItemButton,
-  ListItemText,
-  Stack,
+  ListItem,
+  Skeleton,
   Typography,
 } from "@mui/material";
+import { enqueueSnackbar } from "notistack";
+import { useEffect, useState } from "react";
 
 export default function SavedPage() {
+  const [issues, setIssues] = useState<IssueData[] | null>(null);
+
+  const fetchData = async () => {
+    await fetch("/api/saved/get")
+      .then((res) => res.json())
+      .then((data) => {
+        setIssues(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        enqueueSnackbar("Failed to get saved issues.");
+      });
+  };
+
+  useEffect(() => {
+    if (issues == null) fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [issues]);
+
   return (
     <OverlayPage>
-      <Typography variant="h6">Saved issues</Typography>
+      <Typography variant="h6">Saved</Typography>
       <List sx={{ marginX: "-18px", p: 0 }}>
-        <ListItemButton>
-          <ListItemText>
-            <Stack gap={0.5}>
-              <Typography>Flipped trashcan</Typography>
-              <Stack direction="row" gap={1}>
-                <Chip
-                  icon={<AccessAlarm />}
-                  label="Pending solution"
-                  size="small"
-                  color="info"
-                />
-                <Chip
-                  icon={<DeleteOutline />}
-                  label="Garbage and littering"
-                  size="small"
-                />
-              </Stack>
-            </Stack>
-          </ListItemText>
-        </ListItemButton>
-        <Divider component="li" />
-        <ListItemButton>
-          <ListItemText>
-            <Stack gap={0.5}>
-              <Typography>Flipped trashcan</Typography>
-              <Stack direction="row" gap={1}>
-                <Chip
-                  icon={<AccessAlarm />}
-                  label="Pending solution"
-                  size="small"
-                  color="info"
-                />
-                <Chip
-                  icon={<DeleteOutline />}
-                  label="Garbage and littering"
-                  size="small"
-                />
-              </Stack>
-            </Stack>
-          </ListItemText>
-        </ListItemButton>
+        {issues !== null ? (
+          issues.length > 0 ? (
+            issues.map((value, index) => (
+              <>
+                <Divider key={2 * index} component="li" />
+                <IssueListItem key={2 * index + 1} data={value} />
+              </>
+            ))
+          ) : (
+            <>
+              <Typography align="center" padding="10px 0">
+                No issues saved.
+              </Typography>
+            </>
+          )
+        ) : (
+          [1, 2].map((item, index) => (
+            <>
+              <Divider key={2 * index} component="li" />
+              <ListItem key={2 * index + 1}>
+                <Box sx={{ width: "100%" }}>
+                  <Skeleton variant="text" width="60%" height={30} />
+                  <Skeleton variant="text" width="40%" height={30} />
+                </Box>
+              </ListItem>
+            </>
+          ))
+        )}
       </List>
     </OverlayPage>
   );

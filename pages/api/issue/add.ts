0bd417f4501ed86392291ab72f722668c241aaad
AndyStan112 <stanica.andrei28@gmail.com>
2024-06-21 14:42:32 +0300

    add issue validation with ai

--- pages/api/issue/add.ts
+++ pages/api/issue/add.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prismadb";
+import { issueReviewContext } from "@/constants/openai";
+import axios from "axios";
 
 export default async function handler(
   req: NextApiRequest,
@@ -18,7 +20,25 @@ export default async function handler(
       locationType,
       priority,
     } = req.body;
-    const newIssue = await prisma.issue.create({
+
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      issueReviewContext,
+      {
+        headers: {
+          "content-Type": "application/json",
+          authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+      }
+    );
+    let judgement = JSON.parse(response.data.choices[0].content);
+    console.log(judgement);
+    if (!judgement || !judgement.isSaved || !judgement.description) {
+      console.log("Invalid ai judgement");
+      judgement = { isIssue: null, description: null };
+    }
+
+    let newIssue = await prisma.issue.create({
       data: {
         userId,
         latitude,
@@ -30,8 +50,10 @@ export default async function handler(
         moreDetails,
         statusMessage: "",
         photosUrl: photosUrl,
+        ...judgement,
       },
     });
+
     res
       .status(200)
       .json({ id: newIssue.id, message: "issue successfully created" });

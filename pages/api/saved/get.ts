65e8efdb4b457894dcc9fbac3447dfd5256bf9de
AndyStan112 <stanica.andrei28@gmail.com>
2024-06-22 00:53:53 +0300

    get saved issues

---
+++ pages/api/saved/get.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/lib/prismadb";
+import { getToken } from "next-auth/jwt";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const session = await getToken({ req });
+    const issues = await prisma.savedIssue.findMany({
+      include: { issue: true },
+      where: { userId: session?.sub },
+    });
+
+    res.status(200).json({ issues });
+  } catch (e: any) {
+    res.status(400).json({ error: e.message });
+  }
+}
